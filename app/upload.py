282b41350ac1e45785826e859bb058ab27df038b
mattisavec2t <mattis.almeidalima@gmail.com>
2024-11-21 15:13:30 +0100

    fix: vault.txt formatted

--- app/upload.py
+++ app/upload.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from PyPDF2 import PdfReader
 from io import BytesIO
 
@@ -24,13 +25,16 @@ def extract_text_from_pdf(pdf_stream, output_filename):
             for page in pdf_reader.pages:
                 text = page.extract_text()
                 if text:
-                    output_file.write(text + "\n")
+                    text = text.replace('\n', ' ')
+                    text = re.sub(r'([.!?])\s*', r'\1\n', text)
+                    output_file.write(text)
         print(f"{output_filename} created")
+        
     except Exception as e:
         raise Exception(f"Error during text extraction : {e}")
 
 if __name__ == "__main__":
-    google_drive_link = "https://drive.google.com/file/d/1YWxsSgA0X0M1bI0W4-8VXIUoP10S57I8/view?usp=drive_link"
+    google_drive_link = "https://drive.google.com/file/d/1NguvBe7x4_Vw14AOHnPG3Q9-dgjB7dLs/view?usp=sharing"
     output_txt_path = "vault.txt"
 
     try:
